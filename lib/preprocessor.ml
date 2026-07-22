fb6cc53ae9b95f39d79477d9d1362719fe445eff
Andy Phu Huynh <andyphuhuynh4@gmail.com>
2026-07-22 17:22:46 -0400

    Renamed engine to pp

--- lib/preprocessor.ml
+++ lib/preprocessor.ml
@@ -19,57 +19,55 @@ let get_paths_from_include (current_file : string) (include_ : string) =
 let read_entire_file (name : string) =
   In_channel.with_open_text name In_channel.input_all
 
-let lex_current (engine : t) : Token.t * Lexer.t =
-  Lexer.lex_token engine.source_stack.current.lexer
+let lex_current (pp : t) : Token.t * Lexer.t =
+  Lexer.lex_token pp.source_stack.current.lexer
 
-let lex_current_header_name (engine : t) : Token.t * Lexer.t =
-  Lexer.lex_header_name engine.source_stack.current.lexer
+let lex_current_header_name (pp : t) : Token.t * Lexer.t =
+  Lexer.lex_header_name pp.source_stack.current.lexer
 
-let update_lexer (engine : t) (lexer : Lexer.t) : t =
-  let current = { engine.source_stack.current with lexer } in
-  let source_stack = { engine.source_stack with current } in
-  { engine with source_stack }
+let update_lexer (pp : t) (lexer : Lexer.t) : t =
+  let current = { pp.source_stack.current with lexer } in
+  let source_stack = { pp.source_stack with current } in
+  { pp with source_stack }
 
-let append_source (engine : t) (filepath : string) (contents : string) : t =
+let append_source (pp : t) (filepath : string) (contents : string) : t =
   let source = Source.create_source filepath contents in
-  let source_manager, id = Source.add_source engine.source_manager source in
+  let source_manager, id = Source.add_source pp.source_manager source in
 
   let current = { id; source; lexer = Lexer.create id source } in
   let source_stack =
-    { current; rest = engine.source_stack.current :: engine.source_stack.rest }
+    { current; rest = pp.source_stack.current :: pp.source_stack.rest }
   in
-  { engine with source_manager; source_stack }
+  { pp with source_manager; source_stack }
 
-let pop_source (engine : t) : t =
-  match engine.source_stack.rest with
+let pop_source (pp : t) : t =
+  match pp.source_stack.rest with
   | [] -> failwith "Attempting to pop empty source"
   | x :: xs ->
       let source_stack = { current = x; rest = xs } in
-      { engine with source_stack }
+      { pp with source_stack }
 
-let process_directive_include (engine : t) : t =
-  let token, lexer = lex_current_header_name engine in
-  let engine = update_lexer engine lexer in
+let process_directive_include (pp : t) : t =
+  let token, lexer = lex_current_header_name pp in
+  let pp = update_lexer pp lexer in
   match token.kind with
   | Token.HeaderName { filepath; _ } -> begin
       let filepath =
-        get_paths_from_include engine.source_stack.current.source.filepath
-          filepath
+        get_paths_from_include pp.source_stack.current.source.filepath filepath
       in
       let contents = read_entire_file filepath in
-      append_source engine filepath contents
+      append_source pp filepath contents
     end
   | _ -> begin
       Printf.printf "Expect headername, got: %s"
-        (Token.to_string token engine.source_manager);
+        (Token.to_string token pp.source_manager);
       exit 1
     end
 
-let process_directive (engine : t) : t =
-  let directive, lexer = lex_current engine in
+let process_directive (pp : t) : t =
+  let directive, lexer = lex_current pp in
   match directive.kind with
-  | Identifier "include" ->
-      process_directive_include (update_lexer engine lexer)
+  | Identifier "include" -> process_directive_include (update_lexer pp lexer)
   | Identifier str ->
       Printf.printf "TODO: directive %s" str;
       exit 1
@@ -92,27 +90,26 @@ let init filepath =
     source_stack = { current = { id = source_id; source; lexer }; rest = [] };
   }
 
-let rec next_token (engine : t) : Token.t * t =
-  let token, lexer = lex_current engine in
+let rec next_token (pp : t) : Token.t * t =
+  let token, lexer = lex_current pp in
   match token.kind with
   | Token.Eof ->
-      begin match engine.source_stack.rest with
-      | [] -> (token, engine)
-      | _ -> next_token (pop_source engine)
+      begin match pp.source_stack.rest with
+      | [] -> (token, pp)
+      | _ -> next_token (pop_source pp)
       end
-  | Token.NewLine -> next_token (update_lexer engine lexer)
+  | Token.NewLine -> next_token (update_lexer pp lexer)
   | Token.Hash when token.is_at_line_start ->
-      let engine = process_directive (update_lexer engine lexer) in
-      next_token engine
-  | _ -> (token, update_lexer engine lexer)
+      let pp = process_directive (update_lexer pp lexer) in
+      next_token pp
+  | _ -> (token, update_lexer pp lexer)
 
 let tokenize_all (filepath : string) : Token.t list * Source.manager =
-  let rec helper (engine : t) (acc : Token.t list) :
-      Token.t list * Source.manager =
-    let tok, engine = next_token engine in
+  let rec helper (pp : t) (acc : Token.t list) : Token.t list * Source.manager =
+    let tok, pp = next_token pp in
     match tok.kind with
-    | Token.Eof -> (List.rev (tok :: acc), engine.source_manager)
-    | _ -> helper engine (tok :: acc)
+    | Token.Eof -> (List.rev (tok :: acc), pp.source_manager)
+    | _ -> helper pp (tok :: acc)
   in
-  let engine = init filepath in
-  helper engine []
+  let pp = init filepath in
+  helper pp []
